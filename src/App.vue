5758b7200d29992c4e0db5ff40ea7bcf95aa7dae
TheRose20 <cybrenok91@mail.ru>
2026-05-05 20:12:12 +0300

    Example color change

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,52 @@
 import { TresCanvas } from "@tresjs/core";
 import ExampleScene from "./components/ExampleScene.vue";
 import { OrbitControls } from "@tresjs/cientos";
+import { ref } from "vue";
+import { LinearToneMapping } from "three";
+
+const color = ref("#7dc4e4");
 </script>
 
 <template>
   <div class="container-fluid">
     <div class="row g-0">
-      <div class="col-md-6" style="position: absolute; z-index: 1; color: white; background-color: #181926;">
+      <div
+        class="col-md-6"
+        style="
+          position: absolute;
+          z-index: 1;
+          color: white;
+          background-color: #181926;
+        "
+      >
         <h1 class="display-4">Keyboard Builder</h1>
         <h2 class="display-8">Pre-alpha</h2>
-        <button class="btn btn-primary">Configurate</button>
+
+        <button
+          class="btn btn-primary"
+          @click="color = '#91d7e3'"
+          style="background-color: #91d7e3"
+        >
+          Change color 1
+        </button>
+        <button class="btn btn-secondary" @click="color = '#eed49f'">
+          Change color 2
+        </button>
+        <button class="btn btn-primary" @click="color = '#c6a0f6'">
+          Change color 3
+        </button>
       </div>
-      <!-- <div class="col-md-6"> -->
-        <TresCanvas shadows clear-color="#24273a" window-size>
-          <ExampleScene />
+      <div class="col-md-6">
+        <!-- TODO :fps-limit="60"-->
+        <TresCanvas
+          shadows
+          clear-color="#24273a"
+          window-size
+        >
+          <ExampleScene :color="color" />
           <OrbitControls />
         </TresCanvas>
       </div>
     </div>
-  <!-- </div> -->
+  </div>
 </template>
--- src/components/ExampleScene.vue
+++ src/components/ExampleScene.vue
@@ -1,36 +1,44 @@
 <script setup lang="ts">
 import { useGLTF } from "@tresjs/cientos";
 import { useLoop } from "@tresjs/core";
-import {ref} from "vue";
+import { ref } from "vue";
+
+const props = defineProps({
+  color: String,
+});
 
 const donutRef = ref();
+// const color = ref("#7dc4e4");
 
 const { onBeforeRender } = useLoop();
 
-const modelPath = '/models/exampleKeyboard.glb'
-const { state: model } = useGLTF(modelPath)
+const modelPath = "/models/exampleKeyboard.glb";
+const { state: model } = useGLTF(modelPath);
 
 onBeforeRender(({ elapsed }) => {
-    if (donutRef.value) {
-        donutRef.value.rotation.x = elapsed * .5
-        donutRef.value.rotation.y = elapsed * .5
-    }
-})
-
+  if (donutRef.value) {
+    donutRef.value.rotation.x = elapsed * 0.5;
+    donutRef.value.rotation.y = elapsed * 0.5;
+  }
+});
 </script>
 
 <template>
-    <TresPerspectiveCamera
-        :position="[7 ,7, 7]"
-        :look-at="[0, 0, 0]"
-    />
+  <TresPerspectiveCamera :position="[7, 7, 7]" :look-at="[0, 0, 0]" />
 
-    <TresAmbientLight/>
-    <TresDirectionalLight :position="[0, 2, 4]"/>
+  <TresAmbientLight />
+  <TresDirectionalLight :position="[0, 2, 4]" />
 
-    <!-- <TresAxesHelper/> -->
-    <TresGridHelper/>
+  <!-- <TresAxesHelper/> -->
+  <TresGridHelper />
 
-     <primitive v-if="model" :object="model.scene"/>
-
+  <TresMesh>
+    <primitive v-if="model" :object="model.scene" :ref="donutRef" />
+        <TresMeshStandardMaterial :color="props.color" :metalness="0" />
+  </TresMesh>
+  <TresMesh :cast-shadow="true" :position="[0, 1, 0]">
+    <TresBoxGeometry />
+    <!-- <TresMeshBasicMaterial :color="props.color" /> -->
+        <TresMeshStandardMaterial :color="props.color" :metalness="0" />
+  </TresMesh>
 </template>
